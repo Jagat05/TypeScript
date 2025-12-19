ce8d0e8f2e1a8204508e7f317780f3fcacb399f9
Jagat05 <joshijagaths@gmail.com>
2025-12-19 07:14:05 +0545

    SetUp and Compilation

---
+++ 02_SetupAndCompilation/app.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const a = 10;
+const b = 20;
+const c = 300;
+console.log(`Sum ${a + b + c}`);
+//# sourceMappingURL=app.js.map
---
+++ 02_SetupAndCompilation/app.ts
@@ -0,0 +1,4 @@
+const a:number=10;
+const b:number=20;
+const c:number=300;
+console.log(`Sum ${a+b+c}`);
